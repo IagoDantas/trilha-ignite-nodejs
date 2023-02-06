6ec7a322db98233422a7955d587448c8527e42e1
Iago Dantas <iagodantas.8@gmail.com>
2023-02-06 19:42:31 -0300

    status code / headers / methods

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -2,11 +2,61 @@
 //const http = require('http') //Padrão de importação chamado de COMMONJS (require)
 // APLICAÇÃO HTTP => API 
 //Padrão de importação chamado de ESMODULES6 (import), o import é mais moderno e mais usado, essa    alteração é feita no package.json alterando o type para module.    
+
+//usando o --watch no terminal para ficar observando as alterações no arquivo e atualizar o servidor automaticamente sem a necessidade de reiniciar o servidor ou usar o nodemon
+
+
+
+// ROUTES
+
+// - CRIAR USUARIO
+// - LISTAR TODOS OS USUARIOS
+// - ALTERAR DADOS DO USUARIO
+// - DELETAR USUARIO
+
+// - HTTP 
+//  - MÉTODO HTTP
+//  - URL
+
+// - GET => BUSCAR RECURSO DO BACK-END
+// - POST => INSERIR RECURSO NO BACK-END
+// - PUT => ATUALIZAR ALGUM RECURSO, POR EXEMPLO SE FOR ATUALIZAR VARIAS INFORMAÇÕES POR COMPLETO DO USUARIO(EMAIL, NOME, SENHA, ETC) 
+// - PATCH => ATUALIZAR UMA INFORMAÇÃO ESPECIFICA DE UM RECURSO NO BACK-END 
+// - DELETE => DELETAR UM RECURSO DO BACK-END
+
+
+//stateful => a aplicação depende da informação que é salva em memoria pra funcionar
+//stateless => não salva nada em memoria, salva em dispositivos externos, como banco de dados, por exemplo
+
 import http from 'node:http'// o node pede para colocar um prefixo node: para identificar que é um modulo do node
 
+// Cabeçalhos (Requisição/Resposta) => Metadados 
+
+// HTTP status code => 200, 201, 400, 404, 500
+
+const users = []
+
 const server = http.createServer((req,res)=>{//cria um servidor http e essa função recebe 2 parâmetros, request e response
-    return res.end('Aula do ignite') //retorna uma resposta para o browser com o conteudo Hello World
+    
+    const { method,url } = req
+    
+    if(method === 'GET' && url === '/users'){
+        return res
+        .setHeader('Content-Type','application/json')//seta o cabeçalho da resposta
+        .end(JSON.stringify(users))//ele nao consegue retornar um array, por isso tem que converter para string
+    };
+
+    if(method === 'POST' && url === '/users'){
+
+        users.push({
+            id: 1,
+            name: 'Iago',
+            email: 'iago@example.com',
+        })
+
+        return res.writeHead(201).end()
+    };
+    return res.writeHead(404).end() 
 })
 
 server.listen(3333)
-//usando o --watch no terminal para ficar observando as alterações no arquivo e atualizar o servidor automaticamente sem a necessidade de reiniciar o servidor ou usar o nodemon
