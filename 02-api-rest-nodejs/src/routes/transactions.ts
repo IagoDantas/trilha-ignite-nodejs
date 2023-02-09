2642c627877dd0ce8c1075c1fda314aaa122e230
Iago Dantas <iagodantas.8@gmail.com>
2023-02-08 23:01:16 -0300

    add summary and get transaction by id routes

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -3,7 +3,32 @@ import { FastifyInstance } from "fastify"
 import {knex} from "../database"
 import {z} from 'zod'
 export async function transactionsRoutes(app: FastifyInstance){
-    app.post('/', async (request,reply) => {
+    
+   app.get('/',async () => {
+
+      const transactions = await knex('transactions').select()
+
+      return {transactions}
+   })
+
+   app.get('/summary', async () => {
+      const summary = await knex('transactions').sum('amount',{as:'amount'}).first()
+
+      return {summary};
+   })
+   app.get('/:id', async (request)=>{
+      const getTransactionParamsSchema = z.object({
+         id: z.string().uuid(),
+      })
+
+      const {id} = getTransactionParamsSchema.parse(request.params)
+
+      const transaction = await knex('transactions').where('id',id).first()
+
+      return {transaction}
+   })
+   
+   app.post('/', async (request,reply) => {
 
        const createTransactionBodySchema = z.object({
           title: z.string(),
