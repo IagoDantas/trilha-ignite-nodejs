6427e7d40a55bb7ceea4e3d53b260b6bb47d90d1
Iago Dantas <iagodantas.8@gmail.com>
2023-02-08 22:48:34 -0300

    add http post transaction

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -2,15 +2,15 @@ import {knex as setupKnex, Knex} from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config= {
-    client: 'sqlite',
+    client: 'sqlite3',
     connection: {
         filename: env.DATABASE_URL,
     },
-    useNullAsDefault: true,
     migrations:{
         extension: 'ts',
         directory: './db/migrations'
-    }
+    },
+    useNullAsDefault: true,
 }
 
 export const knex = setupKnex(config)
